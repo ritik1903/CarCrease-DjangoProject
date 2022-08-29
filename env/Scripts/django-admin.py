b6a61271ee65d0e3856b85cd94e579da51cbe219
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2022-08-29 20:56:36 +0530

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\subha\Desktop\Car Crease-Business website\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
